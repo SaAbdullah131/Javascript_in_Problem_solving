a9fdb125a931f1f8e33a243a80f68c4d1adaab3e
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-27 02:51:41 +0600

    largest of from three number and isosceles triangle problem using js

---
+++ largestNumberFromThree.js
@@ -0,0 +1,13 @@
+// find largest number from three number;
+
+var num1=13;
+var num2=79;
+var num3=45;
+
+if(num1>num2 & num1>num3) {
+    console.log(num1,"is largest number");
+}else if(num2>num1 && num2>num3) {
+    console.log(num2,"is largest number");
+}else {
+    console.log(num3, " is largest number");
+}
