0bbf9dd4548a9840de4bbd29524c30dff956b5cf
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-27 02:44:35 +0600

    Grade show based on conditions in js

---
+++ Grade.js
@@ -0,0 +1,68 @@
+var myScore=85;
+
+if(myScore>=80){
+    console.log("A");
+}else if(myScore>=60){
+    console.log("B");
+}else if(myScore>=50){
+    console.log("C");
+}else if(myScore>=40){
+    console.log("D");
+}else {
+    console.log("F");
+}
+var tomScore=66;
+
+if(tomScore>=80){
+    console.log("A");
+}else if(tomScore>=60){
+    console.log("B");
+}else if(tomScore>=50){
+    console.log("C");
+}else if(tomScore>=40){
+    console.log("D");
+}else {
+    console.log("F");
+}
+//jane's score
+var janeScore=95;
+
+if(janeScore>=80){
+    console.log("A");
+}else if(janeScore>=60){
+    console.log("B");
+}else if(janeScore>=50){
+    console.log("C");
+}else if(janeScore>=40){
+    console.log("D");
+}else {
+    console.log("F");
+}
+// peter score
+var peterScore =56;
+
+if(peterScore>=80){
+    console.log("A");
+}else if(peterScore>=60){
+    console.log("B");
+}else if(peterScore>=50){
+    console.log("C");
+}else if(peterScore>=40){
+    console.log("D");
+}else {
+    console.log("F");
+}
+
+//john's score
+var johnScore=40;
+if(johnScore>=80){
+    console.log("A");
+}else if(johnScore>=60){
+    console.log("B");
+}else if(johnScore>=50){
+    console.log("C");
+}else if(johnScore>=40){
+    console.log("D");
+}else {
+    console.log("F");
+}
