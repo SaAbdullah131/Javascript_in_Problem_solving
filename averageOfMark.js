a4efac1d17ef87ac291c716518854ef6c2ce77f0
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-26 16:16:19 +0600

    Average in js

---
+++ averageOfMark.js
@@ -0,0 +1,12 @@
+// Average of one student marks 
+var number1=75.25;
+var number2=65;
+var number3=80;
+var number4=35.45;
+var number5=99.50;
+
+var totalNumber=number1+number2+number3+number4+number5;
+
+var average=totalNumber/5;
+
+console.log(average.toFixed(2));
