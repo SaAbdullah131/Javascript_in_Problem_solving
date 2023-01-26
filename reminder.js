4f54c2ce73ec5d30a8f9b2ea9224946759ff548f
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-26 16:20:27 +0600

    string concatenate and reminder simple problem in js

---
+++ reminder.js
@@ -0,0 +1,5 @@
+// simple reminder problem in js .. very simple
+
+var number1=119;
+var reminder =number1%5;
+console.log(reminder);
---
+++ twoStringConcatenation.js
@@ -0,0 +1,9 @@
+// Two String Concatenation in js 
+
+var statement1="I am going to be";
+var statement2="an awesome web developer";
+
+var fullStatement=statement1+' '+statement2;
+
+console.log(fullStatement);
+
