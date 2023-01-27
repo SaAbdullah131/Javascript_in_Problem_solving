f7126ccecb806d9c278925eeb158d3bf6e96b0cf
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-27 21:08:10 +0600

    checking it's isosceles or not

--- isoscelesTriangle.js
+++ isoscelesTriangle.js
@@ -1,9 +1,10 @@
 // Find the weather is isosceles triangle or not..
+
 var side1=9;
 var side2=8;
 var side3=9;
 
-if((side1===side2) || (side1===side3)){
+if((side1===side2) || (side1===side3) || (side2==side3)){
     console.log("It's an isosceles triangle");
 }else {
     console.log("It's not a isosceles triangle");
