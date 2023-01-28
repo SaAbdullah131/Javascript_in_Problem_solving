ae2b3e9ed6c3cd890aa4ea2e8e601872c297d53f
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-01-28 21:16:37 +0600

    multi stage conditional problem solve

---
+++ buyAppleOrGammingOrLaptop.js
@@ -0,0 +1,14 @@
+// conditional problems in js
+var moneyInMyPocket=80001;
+
+if(moneyInMyPocket>80000){
+    console.log("I will buy a Apple PattiWalla!!");
+}else if(moneyInMyPocket>60000){
+    console.log("I will buy a Gamming Pc ..Chole!");
+}else if(moneyInMyPocket>40000){
+    console.log("I will buy a Lenevo Yoga Laptop");
+}else if(moneyInMyPocket>20000){
+     console.log("I will buy a Second Hand Laptop");
+}else {
+    console.log("Ami Fakira so will buy a walton Phone");
+}
