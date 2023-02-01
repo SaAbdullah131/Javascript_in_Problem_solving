c7ac6ea80005f5c9d97724c6c886fe2020f83ae7
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-02-02 02:56:56 +0600

    string and word reverse in js

---
+++ reverseString.js
@@ -0,0 +1,12 @@
+function reverseString (text) {
+    let reversed ='';
+    text.split(reversed);
+    for(let i=text.length-1;i>=0;i--){
+        reversed = reversed + text[i];
+        console.log(reversed,text[i]);
+    }
+    return reversed;
+}
+const myString = "I am a good boy";
+const reversed = reverseString(myString);
+console.log("reversed output: ",reversed);
---
+++ wordReversed.js
@@ -0,0 +1,16 @@
+// word reverse 
+
+function reversedWord(str) {
+     const words = str.split(' ');
+     const result = [];
+    //  console.log(words);
+    for(let i= words-1 ; i>=0;i--){
+        result.push(words[i]);
+    }
+    const reversed = result.join(' ');
+    return reversed;
+
+}
+const myString = "I am a good boy";
+const reverse = reversedWord(myString);
+console.log(reverse);
